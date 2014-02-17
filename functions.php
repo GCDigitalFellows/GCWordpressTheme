a2e970d157296f33f878de494fa09ab275f29177
Keith Miyake <keith.miyake@gmail.com>
2014-02-16 23:55:20 -0500

    add waypoints to functions.php

--- functions.php
+++ functions.php
@@ -412,7 +412,7 @@ if( !function_exists( "theme_js" ) ) {
 	    wp_register_script( 'wpbs-scripts', get_template_directory_uri() . '/library/js/scripts.js',array('jquery'),'0.1.3', true );
 	    wp_register_script( 'modernizr', get_template_directory_uri() . '/library/js/modernizr.custom.min.js', array(), '2.5.3', true );
 	    wp_register_script( 'shuffle', get_template_directory_uri() . '/library/js/jquery.shuffle.js', array('jquery'), '1.03', true);
-	    wp_register_script( 'waypoints', get_template_directory_uri() . '/library/js/jquery-waypoints/waypoints.min.js', array('jquery'), '2.0.4', true);
+	    wp_register_script( 'waypoints', get_template_directory_uri() . '/library/jquery-waypoints/waypoints.min.js', array('jquery'), '2.0.4', true);
 
 		// only enqueue the following scripts when needed, but register them here to centralize updates.
 		wp_register_script('jquery-ui','//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js', array('jquery'), '1.10.3', true);
