39609aff73647015b505c24e5e6bb78de7c25557
Keith Miyake <keith.miyake@gmail.com>
2014-02-16 23:55:58 -0500

    waypoints!

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -23,10 +23,99 @@ jQuery(window).load(function() {
         offset: jQuery('.navbar').height() + 50 //somewhat arbitrary, might need to be adjusted for various setups
     });
 
-    jQuery('body').scrollspy({
-        target: '.nav-projects',
-        offset: jQuery('.navbar').height() + 50 //somewhat arbitrary, might need to be adjusted for various setups
-    });
+    /* Setup waypoints for triggering events on scroll */
+
+    /* sticky sections
+     *
+     * waypoint-trigger is a class applied to block elements like divs.
+     * there is a waypoint attached to the top and bottom of that element.
+     * when the top waypoint is hit, the "waypoint-sticky" element gets stuck to the top of the screen
+     * when the bottom waypoint is hit, the waypoint-sticky element is unstuck
+     * when the element becomes sticky, a replacement div with the same height is put in place so that the page layout isn't affected.
+     */
+
+    jQuery('.waypoint-trigger').each(function() {
+        jQuery(this).waypoint(function(direction) {
+            var $id = jQuery(this).attr('id');
+            var $nextElement = jQuery('.waypoint-sticky-' + jQuery(this).waypoint('next').attr('id'));
+            if (direction === 'up') {
+                var scrollH = (jQuery.waypoints('viewportHeight') - jQuery('.navbar').height() - jQuery(this).outerHeight(true));
+                if (scrollH < 0) {
+                    jQuery('.waypoint-sticky-' + $id).css('width',jQuery('.waypoint-sticky-' + $id).parent().width());
+                    //jQuery('.waypoint-sticky-' + $id).wrap("<div class='waypoint-wrapper'></div>");
+                    //jQuery('.waypoint-wrapper').css('height',jQuery('.waypoint-sticky-' + $id).height());
+                    //jQuery('.waypoint-wrapper').css('width',jQuery('.waypoint-sticky-' + $id).width());
+                    jQuery('.waypoint-sticky-' + $id).addClass('waypoint-stuck');
+                    jQuery('.waypoint-sticky-' + $id + '.waypoint-stuck').css('top',jQuery('.navbar').height() + 40); //a little margin to breathe
+                    if ($nextElement.hasClass('waypoint-stuck')) {
+                        $nextElement.removeClass('waypoint-stuck');
+                    }
+                }
+            } else {
+                jQuery('.waypoint-sticky-' + $id).removeClass('waypoint-stuck');
+                //jQuery('.waypoint-sticky-' + $id).unwrap();
+            }
+        },{
+          offset: 'bottom-in-view'
+        });
+
+        jQuery(this).waypoint(function(direction) {
+            var $id = jQuery(this).attr('id');
+            var $prevElement = jQuery('.waypoint-sticky-' + jQuery(this).waypoint('prev').attr('id'));
+            var $nextElement = jQuery('.waypoint-sticky-' + jQuery(this).waypoint('next').attr('id'));
+            if (direction === 'up') {
+                jQuery('.waypoint-sticky-' + $id).removeClass('waypoint-stuck');
+                //jQuery('.waypoint-sticky-' + $id).unwrap();
+            } else {
+                var scrollH = (jQuery.waypoints('viewportHeight') - jQuery('.navbar').height() - jQuery(this).outerHeight(true));
+                if (scrollH < 0) {
+                    jQuery('.waypoint-sticky-' + $id).css('width',jQuery('.waypoint-sticky-' + $id).parent().width());
+                    //jQuery('.waypoint-sticky-' + $id).wrap("<div class='waypoint-wrapper'></div>");
+                    //jQuery('.waypoint-wrapper').css('height',jQuery('.waypoint-sticky-' + $id).height());
+                    //jQuery('.waypoint-wrapper').css('width',jQuery('.waypoint-sticky-' + $id).width());
+                    jQuery('.waypoint-sticky-' + $id).addClass('waypoint-stuck');
+                    jQuery('.waypoint-sticky-' + $id + '.waypoint-stuck').css('top',jQuery('.navbar').height() + 40);
+                    if ($prevElement.hasClass('waypoint-stuck')) {
+                        $prevElement.removeClass('waypoint-stuck');
+                    }
+                }
+            }
+        },{
+          offset: jQuery('.navbar').height()
+        });
+    })
+
+    /* hacked up waypoints version of scrollspy since bootstrap only lets you use one per page... */
+
+    jQuery('.waypoint-scrollspy').each(function() {
+        jQuery(this).waypoint(function(direction) {
+            var $currentElement = jQuery(this);
+            var $id = $currentElement.attr('id');
+            var $prevElement = jQuery('.waypoint-scrollspy-' + $currentElement.waypoint('prev').attr('id'));
+            var $nextElement = jQuery('.waypoint-scrollspy-' + $currentElement.waypoint('next').attr('id'));
+            if (direction === 'up') {
+                if ($prevElement.hasClass('hidden')) {
+                    jQuery('.waypoint-scrollspy-' + $id).addClass('hidden');
+                    jQuery('.waypoint-scrollspy-' + $id).removeClass('visible');
+                    
+                    $prevElement.removeClass('hidden');
+                    $prevElement.addClass('visible');
+                }
+            } else {
+                jQuery('.waypoint-scrollspy-' + $id).removeClass('hidden');
+                jQuery('.waypoint-scrollspy-' + $id).addClass('visible');
+                if ($prevElement.hasClass('visible')) {
+                    $prevElement.removeClass('visible');
+                    $prevElement.addClass('hidden');
+                }
+            }
+        },{
+          offset: jQuery('.navbar').height()
+        });
+    })
+
+
+    /* animate scrolling within a page on menu clicks */
 
     jQuery('#main-nav a').click(function() {
 
@@ -43,7 +132,7 @@ jQuery(window).load(function() {
         }, 500);
         return false;
 
-    }); 
+    });
 
     jQuery(window).resize(function() {
         if (jQuery(document).width() < 768) {
@@ -54,6 +143,7 @@ jQuery(window).load(function() {
             jQuery('header.navbar').removeClass('navbar-fixed-top');
             jQuery('body').removeClass('navbar-fixed-offset');
             jQuery('#content').css('margin-top', 0);
+            jQuery.waypoints('disable');
         } else {
             jQuery('.navbar-brand').css('font-size','');
             if (jQuery('body').data('navpos') == 'fixed') {
@@ -69,7 +159,9 @@ jQuery(window).load(function() {
                 jQuery('header.navbar').removeClass('navbar-static-top');
                 jQuery('header.navbar').addClass('navbar-fixed-bottom');
             }
+            jQuery.waypoints('enable');
         }
+        jQuery.waypoints('refresh');
     }).resize();
 
     /* end of as page load scripts */
@@ -77,6 +169,8 @@ jQuery(window).load(function() {
 });
 
 /* additional tools */
+
+
 
 
 /*! A fix for the iOS orientationchange zoom bug.
