ca1c724dd2256eb760c08eeef72d3db5601d07fc
Keith Miyake <keith.miyake@gmail.com>
2014-05-19 15:05:47 -0400

    update waypoints, reduxframework, navwalker to latest master branches

--- library/ReduxFramework/ReduxCore/inc/fields/spinner/field_spinner.js
+++ library/ReduxFramework/ReduxCore/inc/fields/spinner/field_spinner.js
@@ -1,90 +1,85 @@
 /* global redux_change */
-jQuery(document).ready(function() {
 
-    jQuery('.redux_spinner').each(function() {
-        //slider init
-        var spinner = redux.spinner[jQuery(this).attr('rel')];
+(function($) {
+    "use strict";
 
-        jQuery("#" + spinner.id).spinner({
-            value: parseInt(spinner.val, null),
-            min: parseInt(spinner.min, null),
-            max: parseInt(spinner.max, null),
-            step: parseInt(spinner.step, null),
-            range: "min",
-            slide: function(event, ui) {
-                var input = jQuery("#" + spinner.id);
-                input.val(ui.value);
-                redux_change(input);
+    $(document).ready(function() {
+
+        $('.redux_spinner').each(function() {
+            //slider init
+            var spinner = redux.spinner[$(this).attr('rel')];
+
+            $("#" + spinner.id).spinner({
+                value:  parseInt(spinner.val, null),
+                min:    parseInt(spinner.min, null),
+                max:    parseInt(spinner.max, null),
+                step:   parseInt(spinner.step, null),
+                range:  "min",
+
+                slide: function(event, ui) {
+                    var input = $("#" + spinner.id);
+                    input.val(ui.value);
+                    redux_change(input);
+                }
+            });
+
+            // Limit input for negative
+            var neg = false;
+            if (parseInt(spinner.min, null) < 0) {
+                neg = true;
             }
+
+            $("#" + spinner.id).numeric({
+                allowMinus: neg,
+                min:        spinner.min,
+                max:        spinner.max
+            });
+
         });
 
-        // Limit input for negative
-        var neg = false;
-        if (parseInt(spinner.min, null) < 0) {
-            neg = true;
-        }
+        // Update the slider from the input and vice versa
+        $(".spinner-input").keyup(function() {
+            $(this).addClass('spinnerInputChange');
+        });
 
-		jQuery(".spinner-input").numeric({
-			allowMinus: neg,
-			min: spinner.min,
-			max: spinner.max
-		});
+        function cleanSpinnerValue(value, selector, spinner) {
 
-    });
-
-    // Update the slider from the input and vice versa
-    jQuery(".spinner-input").keyup(function() {
-
-        jQuery(this).addClass('spinnerInputChange');
-
-    });
-
-    // Update the slider from the input and vice versa
-    jQuery(".spinner-input").focus(function() {
-
-        if (!jQuery(this).hasClass('spinnerInputChange')) {
-            return;
-        }
-        jQuery(this).removeClass('spinnerInputChange');
-
-        var spinner = redux.spinner[jQuery(this).attr('id')];
-        value = jQuery(this).val();
-        if (value > spinner.max) {
-            value = spinner.max;
-        } else if (value < spinner.min) {
-            value = spinner.min;
-        } else {
-            value = Math.round(value / spinner.step) * spinner.step;
-        }
-
-        jQuery('#' + spinner.id).spinner("value", value);
-
-    });
-
-    jQuery('.spinner-input').typeWatch({
-        callback: function(value) {
-
-            if (!jQuery(this).hasClass('spinnerInputChange')) {
+            if (!selector.hasClass('spinnerInputChange')) {
                 return;
             }
-            jQuery(this).removeClass('spinnerInputChange');
+            selector.removeClass('spinnerInputChange');
 
-            var spinner = redux.spinner[jQuery(this).attr('id')];
-            value = jQuery(this).val();
-            if (value > spinner.max) {
+            if (value === "" || value === null) {
+                value = spinner.min;
+            } else if (value >= parseInt(spinner.max)) {
                 value = spinner.max;
-            } else if (value < spinner.min) {
+            } else if (value <= parseInt(spinner.min)) {
                 value = spinner.min;
             } else {
                 value = Math.round(value / spinner.step) * spinner.step;
             }
 
-            jQuery('#' + spinner.id).spinner("value", value);
+            $("#" + spinner.id).val(value);
 
-        },
-        wait: 400,
-        highlight: false,
-        captureLength: 1
+        }
+
+        // Update the spinner from the input and vice versa
+        $(".spinner-input").blur(function() {
+            //        cleanSpinnerValue(jQuery(this).val(), jQuery(this), redux.spinner[jQuery(this).attr('id')]);
+        });
+
+        $(".spinner-input").focus(function() {
+            cleanSpinnerValue($(this).val(), $(this), redux.spinner[$(this).attr('id')]);
+        });
+
+        $('.spinner-input').typeWatch({
+            callback: function(value) {
+                cleanSpinnerValue(value, $(this), redux.spinner[$(this).attr('id')]);
+            },
+
+            wait:           500,
+            highlight:      false,
+            captureLength:  1
+        });
     });
-
-});
+})(jQuery);
