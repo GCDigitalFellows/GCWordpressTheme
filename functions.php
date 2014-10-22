380231589f791dcdc50762fc864b1b85e12c395d
Keith Miyake <kmiyake@gc.cuny.edu>
2014-10-22 19:18:01 -0400

    update bootstrap js cdn to 3.2.0

--- functions.php
+++ functions.php
@@ -370,7 +370,7 @@ if (!function_exists("theme_js")) {
     function theme_js() {
         if (!is_admin()) {
             //use CDN for loading Bootstrap
-            wp_register_script('bootstrap-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js', array('jquery'), '3.0.3', true);
+            wp_register_script('bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js', array('jquery'), '3.2.0', true);
             wp_register_script('wpbs-scripts', get_template_directory_uri() . '/library/js/scripts.min.js', array('jquery'), '0.1.3', true);
             wp_register_script('modernizr', get_template_directory_uri() . '/library/js/modernizr.custom.min.js', array(), '2.5.3', true);
             wp_register_script('shuffle', get_template_directory_uri() . '/library/js/jquery.shuffle.js', array('jquery'), '1.03', true);
